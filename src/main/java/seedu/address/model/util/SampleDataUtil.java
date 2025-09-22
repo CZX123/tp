b4b5da3a67291bb9a3cb7ea0aaa8435464fd176c
CZX <CZX123@users.noreply.github.com>
2025-09-22 13:40:13 +0800

    Display remark in GUI

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -31,7 +31,7 @@ public class SampleDataUtil {
                 getTagSet("neighbours"), new Remark("Lives next door")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family"), new Remark("Cousin.")),
+                getTagSet("family"), new Remark("Cousin")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
                 getTagSet("classmates"), new Remark("")),
